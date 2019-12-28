6e0095bebb106e89e4d0e6d4ddd4bd215e5709dd
Farhan Ahmed <farhan.ahmed.1@stonybrook.edu>
2019-12-28 16:57:52 -0500

    updated references to pointers

--- point.hpp
+++ point.hpp
@@ -3,14 +3,15 @@
 struct Point {
 	int x;
 	int y;
+	Point(int x, int y);
 };
 
-int Area2(Point& a, Point& b, Point& c);
-bool Left(Point& a, Point& b, Point& c);
-bool LeftOn(Point& a, Point& b, Point& c);
-bool Right(Point& a, Point& b, Point& c);
-bool RightOn(Point& a, Point& b, Point& c);
-bool Collinear(Point& a, Point& b, Point& c);
-bool IntersectProp(Point& a, Point& b, Point& c, Point& d);
-bool Between(Point& a, Point& b, Point& c);
-bool Intersect(Point& a, Point& b, Point& c, Point& d);
+int Area2(Point* a, Point* b, Point* c);
+bool Left(Point* a, Point* b, Point* c);
+bool LeftOn(Point* a, Point* b, Point* c);
+bool Right(Point* a, Point* b, Point* c);
+bool RightOn(Point* a, Point* b, Point* c);
+bool Collinear(Point* a, Point* b, Point* c);
+bool IntersectProp(Point* a, Point* b, Point* c, Point* d);
+bool Between(Point* a, Point* b, Point* c);
+bool Intersect(Point* a, Point* b, Point* c, Point* d);
